4bdb7b57816bf3b8ffabf4fcfc866fd3fbbd2bd8
andremachon <ajbmachon@gmail.com>
2019-02-28 21:52:36 +0100

    Added file for loading Category Tile images and attaching them to Tags

---
+++ server/loadCategoryTagImages.js
@@ -0,0 +1,7 @@
+// Created by Andre Machon 28/02/2019
+import bufferStreamReader from "buffer-stream-reader";
+import Hooks from "@reactioncommerce/hooks";
+import { FileRecord } from "@reactioncommerce/file-collections";
+import { Products } from "/lib/collections";
+import { Media } from "/imports/plugins/core/files/server";
+
